669643b08773fae7cb4cf222f80fd3896c582ef7
jackson <jacksonjay916@gmail.com>
2025-07-20 11:33:44 +0300

    edted urls and views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-
-app_name = 'relationship_app'
+from .views import LibraryDetailView
 
 urlpatterns = [
-    # Function-based view: list all books
-    path('books/', list_books, name='book-list'),
-
-    # Class-based view: show details (and books) for one library
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
 ]
+
+app_name = 'relationship_app'
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,11 @@ from django.views.generic.detail import DetailView
 from .models import Libraryfrom django.views.generic.detail import DetailView
 from .models import Library
 
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
 def list_books(request):
     books = Book.objects.all()  # ✅ This is the missing line
     return render(request, 'relationship_app/list_books.html', {'books': books})
