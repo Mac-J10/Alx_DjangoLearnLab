1af841ffe793b4b8101c9619c2ccd3d18423e9a5
jackson <jacksonjay916@gmail.com>
2025-07-20 11:29:42 +0300

    revised views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Libraryfrom django.views.generic.detail import DetailView
+from .models import Library
 
 def list_books(request):
     books = Book.objects.all()  # ✅ This is the missing line
