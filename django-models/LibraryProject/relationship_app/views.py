f754571e9bc8ab7b50d81d9dfa43e154dccbed55
jackson <jacksonjay916@gmail.com>
2025-07-20 12:21:49 +0300

    user creation form

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render, redirect, HttpResponse
-from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Authentication imports
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Book, Library
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)         # ← UserCreationForm()
+        if form.is_valid():
+            form.save()
+            return redirect('relationship_app:login')
+    else:
+        form = UserCreationForm()                     # ← UserCreationForm()
+
+    return render(
+        request,
+        'relationship_app/register.html',            # ← relationship_app/register.html
+        {'form': form}
+    )
 
 def list_books(request):
     # Fetch all books from the database
