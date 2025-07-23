3d46647c84cf0782acad5d04ee9deb3b789b7dee
jackson <jacksonjay916@gmail.com>
2025-07-23 17:14:01 +0300

    permiissions

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    published_date = models.DateField()
 
-    def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+    class Meta:
+        permissions = [
+            ("can_create", "Can create book"),
+            ("can_delete", "Can delete book"),
+        ]
