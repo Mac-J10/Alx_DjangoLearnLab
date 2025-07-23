48e5ef894c7e8b65356658f4c54138336dd6bfe8
jackson <jacksonjay916@gmail.com>
2025-07-23 17:17:13 +0300

    views update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,11 @@
 from .models import Book
 from django.shortcuts import render, redirect
 from .forms import ExampleForm
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book_view(request):
+    return render(request, 'bookshelf/create_book.html')
 
 def search_books(request):
     query = request.GET.get("q", "")
