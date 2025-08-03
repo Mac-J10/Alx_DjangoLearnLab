516dc6e9d0d4fc513344bd6cec5db90f6c0eac97
jackson <jacksonjay916@gmail.com>
2025-08-03 08:31:27 +0300

    added routing and viewset

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,14 @@
 # api/views.py
 
 from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
