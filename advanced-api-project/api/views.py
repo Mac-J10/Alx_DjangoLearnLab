3b320871313bc07d5572f9cd495b43cbd0e73085
jackson <jacksonjay916@gmail.com>
2025-08-10 10:29:31 +0300

    filters rework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,8 @@ from rest_framework import generics, permissions, filters
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
