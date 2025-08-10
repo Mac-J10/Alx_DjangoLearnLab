01e99e35eb168286376ba2769e2b1d9104c31bc5
jackson <jacksonjay916@gmail.com>
2025-08-10 10:12:06 +0300

    filtering addition

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,8 @@ from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import OrderingFilter
-from rest_framework import generics, permissions, filters
+from rest_framework import generics, permissions
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
