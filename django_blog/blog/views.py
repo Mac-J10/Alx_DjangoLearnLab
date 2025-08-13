77661e91b33581ce9fc52a6cb00cd669be9e5116
jackson <jacksonjay916@gmail.com>
2025-08-13 08:16:45 +0300

    addded blog views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
+
+@login_required
+def profile_view(request):
+    # 1. Check for POST requests
+    if request.method == 'POST':
+        # 2. We pass request.POST into the form
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            # 3. .save() commits the changes to the database
+            form.save()
+            return redirect('blog:profile')
+    else:
+        form = ProfileForm(instance=request.user)
+
+    return render(request, 'blog/profile.html', {'form': form})
