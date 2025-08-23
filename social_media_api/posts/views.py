73e9cb3ec656284d7bb1daaf9b7df9f8e96ef25a
jackson <jacksonjay916@gmail.com>
2025-08-23 18:46:29 +0300

    created and updated  viewsets

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,6 @@
 # posts/views.py
 
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, permissions, filters
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post, Comment
@@ -22,10 +22,14 @@ class PostViewSet(viewsets.ModelViewSet):
     ordering = ['-created_at']
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.select_related('author', 'post')
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    filter_backends = [
+         DjangoFilterBackend,
+        filters.OrderingFilter
+    ]
     filterset_fields = ['post']
     ordering_fields = ['created_at']
     ordering = ['created_at']
