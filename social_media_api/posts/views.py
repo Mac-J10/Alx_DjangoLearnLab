3afe2e8f2cf530673b85856f59dd0141d6f08b03
jackson <jacksonjay916@gmail.com>
2025-08-23 18:48:08 +0300

    updated viewsets handling

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,16 +1,34 @@
 # posts/views.py
 
 from rest_framework import viewsets, permissions, filters
-from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit or delete it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the author of the post/comment.
+        return obj.author == request.user
+
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related('author').prefetch_related('comments')
+    """
+    CRUD for Post objects.
+    Users can only update/delete their own posts.
+    """
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
     filter_backends = [
         DjangoFilterBackend,
         filters.SearchFilter,
@@ -21,13 +39,18 @@ class PostViewSet(viewsets.ModelViewSet):
     ordering_fields = ['created_at', 'updated_at']
     ordering = ['-created_at']
 
+
 class CommentViewSet(viewsets.ModelViewSet):
+    """
+    CRUD for Comment objects.
+    Users can only update/delete their own comments.
+    """
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
     filter_backends = [
-         DjangoFilterBackend,
+        DjangoFilterBackend,
         filters.OrderingFilter
     ]
     filterset_fields = ['post']
