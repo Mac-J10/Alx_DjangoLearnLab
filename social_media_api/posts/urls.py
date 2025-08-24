9c9952842e39c378cd6f52ed66df6e7f012927e3
jackson <jacksonjay916@gmail.com>
2025-08-24 13:14:53 +0300

    like notification implementation

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,11 +3,12 @@
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 from django.urls import path
-from .views import like_post, unlike_post
+from .views import like_post, unlike_post, feed
 
 urlpatterns = [
     path('posts/<int:pk>/like/', like_post, name='like-post'),
     path('posts/<int:pk>/unlike/', unlike_post, name='unlike-post'),
+    path('feed/', feed, name='feed')
 ]
 
 router = DefaultRouter()
